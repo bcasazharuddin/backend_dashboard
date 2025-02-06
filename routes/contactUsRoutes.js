99e57d9e86459d37a627d671b52b70d425bc2551
azharuddin <bcasazharuddin@gmail.com>
2025-02-06 15:45:03 +0530

    add data

--- routes/contactUsRoutes.js
+++ routes/contactUsRoutes.js
@@ -2,7 +2,31 @@ const express = require("express");
 const router = express.Router();
 const contactController = require("../controller/contactUs/contactUs.js")
 
-router.post('/',contactController.contactSave)
+router.post('/',async (req,res)=>{
+    try {
+        
+        const {fname, lname,email,phone,message } = req.body;
+        if(!email){
+            res.status(400).json({ message: "Email is Required", data: "" , success : false , status : 400 });
+        }
+        const contactObj = new contactSchema({
+            fname : fname,
+            lname : lname,
+            email : email,
+            phoneNumber : phone,
+            message : message
+        })
+
+        const contactResult = await contactObj.save();
+        if(contactResult){
+            return res.status(200).json({ message: "Successfully Insert Contact Us Data", success: true, data: contactResult,  status : 200 });
+        }else{
+            res.status(400).json({ message: "Error in database", data: "" , success : false , status : 400 });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", data: "" , success : false , status : 500 });
+    }
+})
 router.get('/',contactController.contactGet)
 
 module.exports = router;
