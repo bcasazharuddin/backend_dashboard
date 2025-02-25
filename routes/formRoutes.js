962125e85ad34075761d3e195cadac66afecac74
Azhar <bcasazharuddin@gmail.com>
2025-02-25 12:31:40 +0530

    add

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -636,7 +636,7 @@ router.get('/searchCruises', async (req, res) => {
         }else if(recommended == "Departure Date (Soonest First)"){
           SortQuery['itinerary.check_in_date.0'] = 1;
         }else if(recommended == "Departure Date (Furthest First)"){
-          SortQuery['itinerary.check_in_date.-1'] = -1;
+          SortQuery['itinerary.check_in_date.0'] = -1;
         }
       }
 
